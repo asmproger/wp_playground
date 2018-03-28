ee367bac0cd2917a5d8593a5e0ce30fddc0a5f2c
sovkutsan <asmprogr@gmail.com>
2018-03-28 18:15:21 +0600

    %)

---
+++ wp-content/plugins/asmproger-plugin/view-plugin.php
@@ -0,0 +1,62 @@
+<?php
+/*
+Plugin Name: Asmproger Plugin
+Plugin URI: https://wordpress2.local
+Description: My first plugin for WP.
+Version: 1.0
+Author: asmproger
+Author URI: http://wasm.in
+*/
+
+// If this file is called directly, abort.
+if (!defined('WPINC')) {
+    die;
+}
+
+/**
+ * Class ViewHelper
+ */
+class ViewHelper
+{
+    private static $_instance = null;
+
+    private function __construct()
+    {
+
+    }
+
+    public static function getInstance()
+    {
+        if (is_null(self::$_instance)) {
+            self::$_instance = new ViewHelper();
+        }
+        return self::$_instance;
+    }
+
+    public function getView(array $params = [], $view = '')
+    {
+        if (!$params) {
+            die('invalid params');
+        }
+        if (!$view) {
+            die('invalid template name');
+        }
+
+        $viewFile = plugin_dir_path(__FILE__) . "/views/{$view}.php";
+
+        if (file_exists($viewFile)) {
+            ob_start();
+            include($viewFile);
+            $content = ob_get_clean();
+
+            foreach ($params as $k => $v) {
+                $k = '{' . $k . '}';
+                $content = str_replace($k, $v, $content);
+            }
+
+            return $content;
+        } else {
+            die('invalid name');
+        }
+    }
+}
---
+++ wp-content/plugins/asmproger-plugin/views/meta.php
@@ -0,0 +1,10 @@
+<table class="book-meta-table">
+    <tr>
+        <td class="book-meta-title">ISBN</td>
+        <td>{isbn}</td>
+    </tr>
+    <tr>
+        <td class="book-meta-title">Author</td>
+        <td>{author}</td>
+    </tr>
+</table>
